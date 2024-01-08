fba1edbc1562fa3c89e4443c50e86ed790c9dd5e
Lemon2311 <marcotighiliu@yahoo.com>
2024-01-08 21:26:03 +0200

    digital pins as output to control relay switch with IoT&mechanicalTwoWaySwitch for the same output work dynamically via list

--- homeServer.js
+++ homeServer.js
@@ -14,8 +14,12 @@ let outputs = [];
 
 let names = [];
 
+let mechanicalInputs = [];
+
+let buttonState = false;
+
 // Function to handle the add-item button click event
-function handleAddItemClick() {
+async function handleAddItemClick() {
   // Get the value of the input field
   const newItemText = inputField.value;
 
@@ -31,10 +35,11 @@ function handleAddItemClick() {
   // Create a new button element
   const newItem = document.createElement("button");
 
-  let name = newItemText.substring(
-    slashIndex + 1,
-    newItemText.length
-  )
+  let name = newItemText.substring(slashIndex + 1, newItemText.length);
+
+  const mechanicalInput = name.substring(name.indexOf("/") + 1, name.length);
+
+  name = name.substring(0, name.indexOf("/"));
 
   // Set the text of the button
   newItem.textContent = name;
@@ -56,29 +61,39 @@ function handleAddItemClick() {
   localStorage.setItem("names", JSON.stringify(names));
 
   // Add a click event listener to the new button, to toggle the output
-  let clickCount = 0;
   newItem.addEventListener("click", function () {
-    clickCount++;
-    if (clickCount % 2 === 1) {
-      outputs[newItem.id].set("high");
+    if (buttonState === false) {
+      console.log(outputs[newItem.id], newItem.id);
+      console.log(outputs);
+      buttonState = true;
     } else {
-      outputs[newItem.id].set("low");
+      buttonState = false;
     }
+    outputs[newItem.id].set(buttonState ? "high" : "low");
   });
-}
 
-let buttonState=false;
+  if (mechanicalInput) {
+    console.log(mechanicalInput);
+    getMechanicalSwitchStateChange(mechanicalInput, out);
+    mechanicalInputs[newItem.id] = mechanicalInput;
+    localStorage.setItem("mechanicalInputs", JSON.stringify(mechanicalInputs));
+  }
+}
 
 // Load the outputs from local storage when the page loads
 window.onload = function () {
   const savedOutputs = JSON.parse(localStorage.getItem("outputs"));
   const savedNames = JSON.parse(localStorage.getItem("names"));
-  console.log(savedOutputs);
-  console.log('dasda');
-  console.log(savedNames);
-  console.log('dasda');
+  const savedMechanicalInputs = JSON.parse(
+    localStorage.getItem("mechanicalInputs")
+  );
 
-  let nameIndex=0;
+  console.log(savedOutputs);
+  console.log("dasda");
+  console.log(savedNames);
+  console.log("dasda");
+
+  let nameIndex = 0;
 
   if (savedOutputs) {
     for (let i = 0; i < savedOutputs.length; i++) {
@@ -88,54 +103,54 @@ window.onload = function () {
       newItem.textContent = savedNames[nameIndex];
       newItem.id = i;
       itemList.appendChild(newItem);
-      const outp = output(savedOutputs[i].ip,savedOutputs[i].type+savedOutputs[i].pin)[savedOutputs[i].type+savedOutputs[i].pin];
+      const outp = output(
+        savedOutputs[i].ip,
+        savedOutputs[i].type + savedOutputs[i].pin
+      )[savedOutputs[i].type + savedOutputs[i].pin];
       outputs.push(outp);
 
       // Add a click event listener to the new button, to toggle the output
       newItem.addEventListener("click", function () {
-        
         if (buttonState === false) {
-          console.log(savedOutputs[newItem.id],newItem.id);
+          console.log(savedOutputs[newItem.id], newItem.id);
           console.log(outputs);
-          buttonState=true;
+          buttonState = true;
         } else {
-          buttonState=false;
+          buttonState = false;
         }
         outputs[i].set(buttonState ? "high" : "low");
       });
+
+    if (savedMechanicalInputs[i]) {
+      getMechanicalSwitchStateChange(savedMechanicalInputs[i], outp);
     }
   }
+}
 };
 
-const { d4, d3 } = input("192.168.1.138", "d4", "d3");
+const getMechanicalSwitchStateChange = async (mechanicalInput, output) => {
+  const inp = input("192.168.1.138", mechanicalInput);
 
-//outputs[0].set("high");
+  let previousValue = await inp[mechanicalInput].get();
 
-let previousValue = await d4.get();
+  setInterval(async () => {
+    inp[mechanicalInput].get().then((currentValue) => {
+      console.log(currentValue, previousValue);
+      console.log("sdadsa");
 
-setInterval(async () => {
-  const currentValue = await d4.get();
-  console.log(currentValue,previousValue);
-  console.log("sdadsa");
+      if (currentValue !== previousValue) {
+        if (buttonState === false) buttonState = true;
+        else buttonState = false;
 
-  if (currentValue !== previousValue) {
-    if (buttonState === false)
-      buttonState = true;
-    else
-      buttonState = false;
+        if (buttonState === true) output.set("high");
+        else output.set("low");
 
-      if(buttonState === true)
-    outputs[0].set('high');
-  else
-    outputs[0].set('low');
-
-  previousValue = currentValue;
-  console.log(currentValue,previousValue);
-
-}
-  
-}, 1000); // Change the interval time as needed
-
+        previousValue = currentValue;
+        console.log(currentValue, previousValue);
+      }
+    });
+  }, 1000); // Change the interval time as needed
+};
 
 //clear  local storage  for testing
 //localStorage.clear();
